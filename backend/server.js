42bbad61e6daf9b6d6b1bbb5776f39c323f55e86
Roberto Rodas-Herndon <rrh2023@bu.edu>
2022-03-23 17:22:26 -0400

    Cleaned up our folders in preperation for development

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// connect to mongoDB
+// const mongoUri = "some url"
+// mongoose.connect(mongoUri)
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => {
+    console.log("Express server is running on port 3001")
+})
