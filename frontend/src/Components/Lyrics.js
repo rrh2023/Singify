f81a038bfc370242f425d811465e62be6d04a934
Roberto Rodas-Herndon <rrh2023@bu.edu>
2022-04-01 09:29:14 -0400

    Made changes

--- frontend/src/Components/Lyrics.js
+++ frontend/src/Components/Lyrics.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Lyrics = () => {
+  
+  const [term, setTerm] = useState({
+    term: ''
+  })
+
+  const [songs, setSongs] = useState([])
+
+  const handleChange = (e) => {
+
+    const value = e.target.value
+    setTerm(()=> {
+      return (
+        {
+          term: value
+        }
+      )
+    })
+    console.log(term.term)
+  } 
+
+  async function search(e){
+    e.preventDefault() // do not refresh page
+    let searchTerm = term.term
+    console.log('our term', searchTerm)
+
+    let res = await fetch(`https://api.lyrics.ovh/suggest/${searchTerm}`)
+    let data = await res.json()
+    setSongs(data.data)
+    console.log(songs)
+    setTerm(() => { // term is empty again
+      return (
+        {
+          term: '',
+        }
+      )
+    })
+
+  }
+
   return (
-    <h1>I'm the Lyrics.vho page!</h1>
+    <div>
+      <h1>I'm the Lyrics.vho page!</h1>
+
+      <form>
+        <input type="text" name="term" onChange={handleChange} value={term.term}/>
+        <input type="submit" value="Search" onClick={search}/>
+      </form>
+      
+      <h3>API Results</h3>
+      {
+        songs.length === 0 ? 
+        <p style={{color: 'red'}}>No results...</p> 
+        :
+        songs.map(
+          song => {
+            return (
+              <li key={song.id}>
+                <span><strong>{song.artist.name}</strong> - {song.title} </span>
+                <button>Get Lyrics</button>
+              </li>
+            )
+          } 
+
+        )
+      }
+    
+      
+    </div>
   )
 }
 
